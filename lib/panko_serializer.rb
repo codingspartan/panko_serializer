23043620a15fb99b14eecb54adf3d06c7e7b5d41
René Deproge <rene.deproge@thetrainline.com>
2023-01-02 17:42:56 +0100

    Fix benchmarks

--- lib/panko_serializer.rb
+++ lib/panko_serializer.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
-require "panko/version"
-require "panko/attribute"
-require "panko/association"
-require "panko/serializer"
-require "panko/array_serializer"
-require "panko/response"
-require "panko/serializer_resolver"
-require "panko/object_writer"
+require_relative "panko/version"
+require_relative "panko/attribute"
+require_relative "panko/association"
+require_relative "panko/serializer"
+require_relative "panko/array_serializer"
+require_relative "panko/response"
+require_relative "panko/serializer_resolver"
+require_relative "panko/object_writer"
 
 # C Extension
 require "oj"
-require "panko/panko_serializer"
